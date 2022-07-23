529acc80fd380498df4a0f39252a399cc2987428
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:06:42 +0800

    Update Code15.py

--- Code15.py
+++ Code15.py
@@ -1,1 +1,10 @@
 
+# 查看张量的维数
+from PIL import Image
+import torchvision.transforms as transforms
+
+img = Image.open("F:/photo/grocery store/photo01.jpg")
+img = transforms.ToTensor()(img)
+
+print(img.size)
+print(img.shape)
