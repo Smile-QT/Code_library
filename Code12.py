ba8ac4f91b1c3525f713beee51590f0c756c15f6
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:05:43 +0800

    Update Code12.py

--- Code12.py
+++ Code12.py
@@ -1,1 +1,11 @@
 
+# transforms.ToTensor() 将给定图像转为Tensor
+from PIL import Image
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+
+img = Image.open("F:/photo/grocery store/photo01.jpg")
+plt.imshow(img)
+plt.show()
+img = transforms.ToTensor()(img)
+print(img)
