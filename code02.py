30e65111db6c60654787e1dcf92c59157a8b9d86
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 19:45:04 +0800

    Update code02.py

--- code02.py
+++ code02.py
@@ -1,1 +1,8 @@
+# 将张量保存为图片
+import torch
+from torchvision import transforms
 
+toPIL = transforms.ToPILImage()  # 这个函数可以将张量转为PIL图片，由小数转为0-255之间的像素值
+img = torch.randn(3, 128, 64)
+pic = toPIL(img)
+pic.save('C:\LB\Restormer-masterpyV3\YXL/random.jpg')
