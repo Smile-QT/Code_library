9754b710bf7821020827207428ab754bf64316f4
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:14:40 +0800

    Update Code34.py

--- Code34.py
+++ Code34.py
@@ -1,1 +1,14 @@
 
+# 查看张量的维度
+# Tensor类的成员函数dim()可以返回张量的维度，shape属性与成员函数size()返回张量的具体维度分量
+import torch
+f = torch.randn(2, 3)
+print(f)
+print(f.dim())
+print(f.size())
+print(f.size(0))
+print(f.size(1))
+print(f.shape)
+print(f.shape)
+
+
