fada9e261170debd05d223e52c648998d68bb685
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:15:31 +0800

    Update Code36.py

--- Code36.py
+++ Code36.py
@@ -1,1 +1,21 @@
 
+# range torch.range torch.arange区别
+# torch.range torch.arange最后的结果的张量
+
+import torch
+y = torch.range(1, 6)
+print(y)
+print(y.dtype)
+print(y.type())
+
+
+z = torch.arange(1, 6)
+print(z)
+print(z.dtype)
+print(z.type())
+
+
+a = list()
+a = range(5)
+for i in a:
+    print(i)
