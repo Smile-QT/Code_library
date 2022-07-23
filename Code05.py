9a7383a722d64cca63acd62e8f7cecfdd56a3f6c
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 20:58:33 +0800

    Update Code05.py

--- Code05.py
+++ Code05.py
@@ -1,1 +1,12 @@
 
+# 把list类型和numpy类型转换成tensor类型
+import torch
+import numpy
+from typing import List
+
+image01 = list([1, 2, 3, 4])
+image02 = numpy.array([1, 2, 3, 4])
+images01 = torch.as_tensor(image01)
+images02 = torch.as_tensor(image02)
+print(images01)
+print(images02)
