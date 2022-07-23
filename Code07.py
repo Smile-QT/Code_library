8bd3e6c175a29f016038516ff2a86988448e4da2
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:00:16 +0800

    Update Code07.py

--- Code07.py
+++ Code07.py
@@ -1,1 +1,19 @@
 
+# 读取并显示一张图片
+# 我感觉imread所有格式图片都可以读取
+import cv2
+img = cv2.imread('F:/photo/grocery store/photo01.jpg')
+cv2.namedWindow("img", 0)
+cv2.resizeWindow("img", 300, 300)  # 设置窗口大小
+cv2.imshow('img', img)
+cv2.waitKey(0)  # 防止图片闪退
+
+# 读取并显示一张图片
+# 我感觉imread所有格式图片都可以读取
+import cv2
+img = cv2.imread('F:/photo/grocery store/photo01.jpg')
+cv2.namedWindow("img", 0)
+cv2.resizeWindow("img", 300, 300)  # 设置窗口大小
+cv2.imshow('img', img)
+cv2.waitKey(0)  # 防止图片闪退
+
