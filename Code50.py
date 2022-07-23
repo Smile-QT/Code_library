41026845d90e22e43781e26c5584263e8f314ccd
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:29:53 +0800

    Update Code50.py

--- Code50.py
+++ Code50.py
@@ -1,1 +1,40 @@
 
+# repeat()
+
+import torch
+x = torch.tensor([1, 2, 3])
+print(x.repeat(4))
+print("###################################")
+print(x.repeat(4, 1))
+
+
+import torch
+x = torch.tensor([1, 2, 3])
+print(x.repeat(4, 1))
+print("###################################")
+print(x.repeat(4, 2))
+
+
+
+import torch
+x = torch.tensor([1, 2, 3])
+print(x.repeat(4, 1))
+print("###################################")
+print(x.repeat(4, 2, 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
