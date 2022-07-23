c83aba413930e0fb6bf8bcf9fdab36984667fd8d
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:08:04 +0800

    Update Code19.py

--- Code19.py
+++ Code19.py
@@ -1,1 +1,14 @@
 
+# transforms.RandomRotation(degrees=15) 随机旋转
+from PIL import Image
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import numpy as np
+
+img = Image.open("F:/photo/grocery store/photo01.jpg")
+img = transforms.ToTensor()(img)
+(img) = transforms.RandomRotation(degrees=15)(img)
+img = np.transpose(img, (1, 2, 0))
+plt.imshow(img)
+plt.show()
+
