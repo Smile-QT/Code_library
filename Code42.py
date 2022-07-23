820fdabaef4018df69b5132a9bfd3c3fbb72edd2
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:23:07 +0800

    Update Code42.py

--- Code42.py
+++ Code42.py
@@ -1,1 +1,48 @@
 
+# unsqueeze()函数、squeeze()函数介绍
+
+import torch
+# 1. unsqueeze()函数
+a = torch.ones(3, 4)
+b = a.unsqueeze(0)
+print(a)
+print(b)
+print(b.size())
+
+c = a.unsqueeze(1)
+print(c)
+print(c.size())
+
+# 2. squeeze()函数 只有维度为1时才能去掉
+d = a.unsqueeze(1)
+print(d)
+print(d.size())
+
+e = d.squeeze(1)
+print(d)
+print(e)
+print(e.size())
+
+# 只有维度为1时才能去掉
+f = d.squeeze(0)
+print(f)
+print(f.size())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
