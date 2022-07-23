b5fef5fc9f9dea4e1e665f11be8cd346f3dd1edf
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:24:58 +0800

    Update Code46.py

--- Code46.py
+++ Code46.py
@@ -1,1 +1,14 @@
 
+# 显示一张图片
+import matplotlib.pyplot as plt  # plt 用于显示图片
+from PIL import Image
+
+img1 = Image.open("F:/Graduate students grade three/postgraduate studies/first year of postgraduate study/Transformer/softmax.jpg")
+
+#结果展示
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 中文乱码
+plt.subplot(121)
+plt.imshow(img1)
+plt.title('图像1')
+plt.axis('off')
+plt.show()
