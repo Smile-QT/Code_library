e2f2913e38753df577ccdbda04c9f2e34ae9f9eb
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:23:41 +0800

    Update Code43.py

--- Code43.py
+++ Code43.py
@@ -1,1 +1,40 @@
 
+# 将numpy.ndarray变成一个张量
+# torch.from_numpy(ndarray) → Tensor，
+# 即 从numpy.ndarray创建一个张量。
+
+import torch
+import numpy as np
+
+a = np.array([1, 2, 3])
+t = torch.from_numpy(a)
+print(type(a))
+print(type(t))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
