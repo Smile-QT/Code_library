468653015001b3eda54f1b87816a221dcd99e678
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:13:32 +0800

    Update Code31.py

--- Code31.py
+++ Code31.py
@@ -1,1 +1,6 @@
 
+# enumerate对于字典的使用，i是字典的值，w是字典的键
+tgt_vocab = {'P' : 0, 'i' : 1, 'want' : 2, 'a' : 3, 'beer' : 4, 'coke' : 5, 'S' : 6, 'E' : 7, '.' :8}
+idx2word = {i: w for i, w in enumerate(tgt_vocab)}
+print(idx2word)
+
