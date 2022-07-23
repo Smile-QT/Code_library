29bf27c5cbfdfb7e1f8c2e6629bef8afc701f9cf
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:07:00 +0800

    Update Code16.py

--- Code16.py
+++ Code16.py
@@ -1,1 +1,8 @@
 
+# 将图片数据转化成张量
+from PIL import Image
+import torchvision.transforms as transforms
+
+img = Image.open("F:/photo/grocery store/photo01.jpg")
+img = transforms.ToTensor()(img)
+print(type(img))
