7455da7dd84e24ad4a2c9b6ce352c42745b50c50
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:07:48 +0800

    Update Code18.py

--- Code18.py
+++ Code18.py
@@ -1,1 +1,13 @@
 
+# 调整张量的维度顺序
+from PIL import Image
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import numpy as np
+
+img = Image.open("F:/photo/grocery store/photo01.jpg")
+img = transforms.ToTensor()(img)
+img = np.transpose(img, (1, 2, 0))
+plt.imshow(img)
+plt.show()
+
