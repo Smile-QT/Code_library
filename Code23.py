bdc3e1618a8dfbdaa8ca27711893d934a05398c5
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:09:52 +0800

    Update Code23.py

--- Code23.py
+++ Code23.py
@@ -1,1 +1,40 @@
 
+# 张量转化成数组
+import numpy as np
+import torch
+
+a = torch.Tensor([0,1]) # 如果参数不是一个list，而是一个int，将会返回该int长的内容未定义的Tensor
+print(type(a))
+a = a.numpy()
+print(type(a))
+
+
+# 数组转化成张量
+b = np.random.rand(3)
+print(type(b))
+b = torch.from_numpy(b)
+print(type(b))
+
+
+# 列表转化成张量
+c = [1, 2]
+print(type(c))
+c = torch.tensor(c)
+print(type(c))
+
+
+
+# 列表转化成数组
+d = [1, 2]
+print(type(d))
+d = np.array(d)
+print(type(d))
+
+
+
+
+
+
+
+
+
