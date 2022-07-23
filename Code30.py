66dde5cf98e86927cbe687eaf9ff9d0677f18a1a
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:13:13 +0800

    Update Code30.py

--- Code30.py
+++ Code30.py
@@ -1,1 +1,10 @@
 
+#  混淆矩阵
+cm = confusion_matrix(y_test, y_pred)  # 计算TP, FP, TN, FN
+df_cm = pd.DataFrame(cm, index = class_names, columns = class_names)
+hmap = sns.heatmap(df_cm, annot = True)
+hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation = 0, ha = 'right' )
+hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation = 0, ha = 'right' )
+plt.ytable('True Label')
+plt.ytabel('Pred Label')
+plt.show()
