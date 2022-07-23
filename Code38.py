032a01bcfe0cc8f263e7f44e297cfb1d35d2676a
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:16:31 +0800

    Update Code38.py

--- Code38.py
+++ Code38.py
@@ -1,1 +1,18 @@
 
+# 用numpy创建一维数组
+# dtype：查看数据结构中保存到的数据类型
+# type：查看数据结构类型
+import numpy as np
+
+array_1d = np.ones(3)
+print(array_1d)
+print(array_1d.dtype)
+print(type(array_1d))
+
+a = list()
+a = [1, 1, 1]
+print(a)
+print(type(a))
+
+
+
