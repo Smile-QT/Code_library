57a21f6fd8861646ba69b66fa89fbd4fdbb5010b
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:04:34 +0800

    Update Code09.py

--- Code09.py
+++ Code09.py
@@ -1,1 +1,19 @@
 
+# python三种输出格式
+# 第一种  python—print(f “{}”) 的用法
+a = 1
+b = 2
+S = a + b
+P = a * b
+print(f"Sum of a and b is {S}, and product is {P}")
+
+# 第二种
+print('S的值：', S)
+print('P的值：', P)
+
+# 第三种
+print('S的值： {0}，P的值{1} '.format(S, P))
+print('S的值： {}，P的值{} '.format(S, P))
+print('S的值： {1}，P的值{0} '.format(S, P))
+
+
