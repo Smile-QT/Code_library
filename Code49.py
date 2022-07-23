42ebfea61dcd83609851f57e069a748e91456959
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-07-23 21:29:08 +0800

    Update Code49.py

--- Code49.py
+++ Code49.py
@@ -1,1 +1,20 @@
 
+# 迁移学习，测试模型的加载
+
+# 测试模型的加载
+model = resnet34(num_classes=5)
+# load model weights
+model_weight_path = "./resNet34.pth"
+model.load_state_dict(torch.load(model_weight_path, map_location=device))
+model.eval()
+
+
+
+
+
+
+
+
+
+
+
