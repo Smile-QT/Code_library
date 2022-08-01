8694a36e0a8ec5ea6b948818eca12131ce3d39f7
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-08-01 23:21:04 +0800

    Update Code54.py

--- Code54.py
+++ Code54.py
@@ -1,4 +1,7 @@
-# Tensor 和 NumPy变量互相转化
+# Tensor 和 NumPy 变量互相转化
+# tuple 和 list 变量相互转化
+
+
 # 1. Tensor 转 NumPy
 import torch
 a = torch.ones(6)
@@ -15,6 +18,17 @@ b1 = torch.from_numpy(a1)
 print(a1.dtype)
 print(b1.dtype)
 
+
+# 3. list 转 tuple
+list01 = [1, 2, 3]
+tu01 = tuple(list01)
+print(type(tu01))
+
+
+# 4. tuple 转 list
+tu02 = (1, 2, 3, 4)
+list02= list(tu02)
+print(type(list02))
 
 
 
