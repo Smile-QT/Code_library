9aac1c9ab6d0b99047e7e7c8af8165d4246ae10c
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-08-01 18:53:11 +0800

    Update Code54.py

--- Code54.py
+++ Code54.py
@@ -1,1 +1,31 @@
+# Tensor 和 NumPy变量互相转化
+# 1. Tensor 转 NumPy
+import torch
+a = torch.ones(6)
+b = a.numpy()
+print(a.dtype)
+print(b.dtype)
+
+
+# 2. NumPy 数组转 Tensor
+
+import numpy as np
+a1 = np.ones(7)
+b1 = torch.from_numpy(a1)
+print(a1.dtype)
+print(b1.dtype)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
