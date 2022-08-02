db2b3b735860197ffa8e1f83455632613b096e1e
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-08-02 17:15:58 +0800

    Update Code66.py

--- Code66.py
+++ Code66.py
@@ -1,24 +1,34 @@
 
 # 在pytorch中实现将sobel算子和卷积层结合来提取图像中物体的边缘轮廓图
 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#@Time : 2022/6/2 13:36
+#@Author : Smile
+#@File : code1_2.py
+#@Software: PyCharm
+@Description:
+"""
+
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
 from torch import nn
 import cv2
 
-
 def edge_conv2d(im):
     """
     使用卷积层对图像进行边缘轮廓提取
-    
+
     Parameters
     ----------
     im
-    待处理的图片
+        待处理的图片
+
     Returns
     -------
-    返回卷积后的图片
+        返回卷积后的图片
     """
     # 用nn.Conv2d定义卷积操作
     conv_op = nn.Conv2d(3, 3, kernel_size=3, padding=1, bias=False)
@@ -36,18 +46,20 @@ def edge_conv2d(im):
     # 将输出转换为图片格式
     edge_detect = edge_detect.squeeze().detach().numpy()
     return edge_detect
+
 def edge_extraction(src_path: str, result_path: str):
     """
     使用sobel算子和卷积层结合来提取图像中物体的边缘轮廓图
+
     Parameters
     ----------
     src_img_path：str
-    原始图片的保存位置
+        原始图片的保存位置
     result_img_path: str
-    转化后的图片的保存位置
+        转化后的图片的保存位置
     Returns
     -------
-    没有返回值
+        没有返回值
     """
     im = cv2.imread(src_path, flags=1)
     im = np.transpose(im, (2, 0, 1))
@@ -61,23 +73,20 @@ def edge_extraction(src_path: str, result_path: str):
     plt.savefig(result_path)
     # 将转化为RGB格式的图片保存到result_img_path位置
     plt.show()
+
 def main():
     src01_img_path = '../Photo/src01_picture2_2.jpg'
     result01_img_path = '../Photo/result01_picture2_2.jpg'
+
     src02_img_path = '../Photo/src02_picture2_2.jpg'
     result02_img_path = '../Photo/result02_picture2_2.jpg'
+
     edge_extraction(src01_img_path, result01_img_path)
     edge_extraction(src02_img_path, result02_img_path)
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
 
 
 
