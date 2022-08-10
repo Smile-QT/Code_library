33043ec99fb12a855d6b3ba29bbff91028e349e7
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-08-10 21:16:36 +0800

    Update Code71.py

--- Code71.py
+++ Code71.py
@@ -1,1 +1,18 @@
 
+
+
+# Python遍历文件夹下所有文件，并返回文件绝对地址列表
+
+import os
+
+def dirlist(path):  
+    allfile = []
+    filelist =  os.listdir(path)  
+  
+    for filename in filelist:  
+        filepath = os.path.join(path, filename)  
+        if os.path.isdir(filepath):  
+            dirlist(filepath, allfile)  
+        else:  
+            allfile.append(filepath)  
+    return allfile
