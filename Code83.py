6beb11157044e0e3feba6333a936e5cc68a1f289
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-09-08 21:22:25 +0800

    Update Code83.py

--- Code83.py
+++ Code83.py
@@ -1,1 +1,18 @@
 
+# 获取一个指定文件夹下所有文件的完整路径
+
+import os
+
+
+def read_file(path):  # 图片的完整路径
+    """从文件夹中读取数据"""
+    files_list = os.listdir(path)
+    file_path_list = [os.path.join(path, img) for img in files_list]
+    file_path_list.sort()  # 图片路径排序
+    return file_path_list
+
+file_path01 = 'C:/YXL/lijiawen/reptile/shipingzimu/tiquzimu/Data/video/'
+file_path_list = read_file(file_path01)
+for i in range(len(file_path_list)):
+    print(file_path_list[i])
+
