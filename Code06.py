af01eb1aab37086706f36345359042de99963614
Smile-QT <47960293+Smile-QT@users.noreply.github.com>
2022-09-18 17:18:20 +0800

    Update Code06.py

--- Code06.py
+++ Code06.py
@@ -1,4 +1,4 @@
-
+# 方法一
 # 将若干张图片拼成一张图片展示
 # 展示出来的图片的宽度：nrow, 高度：len(imge)/nrow
 import torch
@@ -36,6 +36,64 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# 方法二
+# sphinx_gallery_thumbnail_path = "../../gallery/assets/visualization_utils_thumbnail2.png"
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torchvision.transforms.functional as F
+
+
+plt.rcParams["savefig.bbox"] = 'tight'
+
+
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    plt.show()
+
+
+from torchvision.utils import make_grid
+from torchvision.io import read_image
+from pathlib import Path
+
+dog1_int = read_image(str('C:\YXL\papers\Subjective_Evaluation\LOL/fourth_photo\circle/780.png'))
+dog2_int = read_image(str('C:\YXL\papers\Subjective_Evaluation\LOL/fourth_photo\circle/780.png'))
+dog_list = [dog1_int, dog2_int]
+
+grid = make_grid(dog_list)
+show(grid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
